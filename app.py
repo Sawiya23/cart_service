33abb936e3d2ed8c8e82d0dc78a15a6f6e7927da
Sawiya23 <112042305+Sawiya23@users.noreply.github.com>
2024-10-01 08:53:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
 import requests
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # Enable CORS for all routes
 
 # Simulating user carts stored in memory
 cart = {}
